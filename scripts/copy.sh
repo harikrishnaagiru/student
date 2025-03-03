b450d71a43f9c0a2f880f28bb324b007581f85b5
harikrishna <118521049+harikrishnaagiru@users.noreply.github.com>
2025-03-03 05:50:34 +0530

    Create copy.sh

---
+++ scripts/copy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Removing old JAR files..."
+rm -f /home/ubuntu/studentapp/*.jar
+
+echo "Copying new JAR file from S3..."
+aws s3 cp s3://studentattendanceapp/StudentAttendanceApp-1.0.jar /home/ubuntu/studentapp/
+
+echo "Setting permissions for the JAR file..."
+chmod +x /home/ubuntu/studentapp/StudentAttendanceApp-1.0.jar
