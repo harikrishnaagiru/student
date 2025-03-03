643bae873d55b569905d479bb4b812e92ec49388
harikrishna <118521049+harikrishnaagiru@users.noreply.github.com>
2025-03-03 05:52:42 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "Stopping existing Java application..."
+sudo pkill -f StudentAttendanceApp-1.0.jar || echo "No process found"
+
 echo "Starting new Java application..."
 nohup java -jar /home/ubuntu/studentapp/StudentAttendanceApp-1.0.jar > /home/ubuntu/studentapp/app.log 2>&1 &
